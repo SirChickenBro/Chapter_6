75bb285478980cf7462d95f29500f0934a4282f0
SirChickenBro <freddiewashington5555@gmail.com>
2026-01-28 12:38:49 -0700

    item collection update

--- Chapter_6_FredWashington/Assets/Scripts/GameBehavior.cs
+++ Chapter_6_FredWashington/Assets/Scripts/GameBehavior.cs
@@ -1,11 +1,57 @@
+using TMPro;
 using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
+    private int _itemsCollected = 0;
+    private int _playerHP = 10;
+    public int MaxItems = 4;
+
+    public TMP_Text HealthText;
+    public TMP_Text ItemText;
+    public TMP_Text ProgressText;
+
+    // 1
+    public int Items
+    {
+        // 2
+        get { return _itemsCollected; }
+        // 3
+        set
+        {
+            _itemsCollected = value;
+            ItemText.text = "Items: " + Items;
+            if (_itemsCollected >= MaxItems)
+            {
+                ProgressText.text = "You've found all the items!";
+            }
+            else
+            {
+                ProgressText.text = "Item found, only " +
+                    (MaxItems - _itemsCollected) + " more!";
+            }
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+        }
+    }
+    // 4
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            HealthText.text = "Health: " + HP;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ItemText.text += _itemsCollected;
+        HealthText.text += _playerHP;
     }
 
     // Update is called once per frame
--- Chapter_6_FredWashington/Assets/Scripts/ItemBehavior.cs
+++ Chapter_6_FredWashington/Assets/Scripts/ItemBehavior.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class ItemBehavior : MonoBehaviour
 {
     // 1
+    public GameBehavior GameManager;
+    void Start()
+    {
+        // 2
+        GameManager = GameObject.Find("Game Manager")
+            .GetComponent<GameBehavior>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         // 2
@@ -13,6 +20,8 @@ public class ItemBehavior : MonoBehaviour
             Destroy(this.transform.gameObject);
             // 4
             Debug.Log("Item collected!");
+
+            GameManager.Items += 1;
         }
     }
 }
